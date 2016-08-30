b4a9b6244ccf38852d0518ee39c9f691691bf30b
jcartwright <jason@ensightenterprises.com>
2016-08-30 16:47:46 -0500

    authorization headers

--- lib/rally_api.ex
+++ lib/rally_api.ex
@@ -1,2 +1,14 @@
 defmodule RallyApi do
+  # alias RallyApi.Client
+
+  def authorization_header(%{user: user, password: password}, headers) do
+    userpass = "#{user}:#{password}"
+    headers ++ [{"Authorization", "Basic #{:base64.encode(userpass)}"}]
+  end
+
+  def authorization_header(%{zsessionid: api_key}, headers) do
+    headers ++ [{"ZSESSIONID", api_key}]
+  end
+
+  def authorization_header(_, headers), do: headers
 end
--- test/rally_api_test.exs
+++ test/rally_api_test.exs
@@ -1,8 +1,19 @@
 defmodule RallyApiTest do
   use ExUnit.Case
+  import RallyApi
+
   doctest RallyApi
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "authorization_header using user and password" do
+    assert authorization_header(%{user: "user", password: "password"}, []) == [{"Authorization", "Basic dXNlcjpwYXNzd29yZA=="}]
+  end
+
+  test "authorization_header using api key" do
+    assert authorization_header(%{zsessionid: "ZSESSIONID"}, []) == [{"ZSESSIONID", "ZSESSIONID"}]
+  end
+
+  test "authorization_header pass-through" do
+    headers = [{"X-RallyIntegrationName", "RallyRestToolkitForElixir"}]
+    assert authorization_header(%{unknown: "barbaz"}, [{"X-RallyIntegrationName", "RallyRestToolkitForElixir"}]) == headers
   end
 end

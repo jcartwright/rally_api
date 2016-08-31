fbacde90e252aa4f531bd8dfef2cc8f8c13e114d
jcartwright <jason@ensightenterprises.com>
2016-08-30 21:29:37 -0500

    Add @doc to authorization_header function

--- lib/rally_api.ex
+++ lib/rally_api.ex
@@ -1,6 +1,25 @@
 defmodule RallyApi do
   # alias RallyApi.Client
 
+  @doc """
+  There are two ways to authenticate through the Rally REST API v2:
+
+    * Basic authentication
+    * API Key
+
+  This function accepts both.
+
+  ## Examples
+
+    iex> RallyApi.authorization_header(%{user: "user", password: "password"}, [])
+    [{"Authorization", "Basic dXNlcjpwYXNzd29yZA=="}]
+
+    iex> RallyApi.authorization_header(%{zsessionid: "0192837465"}, [])
+    [{"ZSESSIONID", "0192837465"}]
+
+  ## More info
+  https://rally1.rallydev.com/slm/doc/webservice/authentication.jsp
+  """
   def authorization_header(%{user: user, password: password}, headers) do
     userpass = "#{user}:#{password}"
     headers ++ [{"Authorization", "Basic #{:base64.encode(userpass)}"}]

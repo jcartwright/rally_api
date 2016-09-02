18f7d8cb558c0f9cf9ef8288231805e071fbea98
jcartwright <jason@ensightenterprises.com>
2016-09-02 16:19:49 -0500

    Add exvcr for tests

--- mix.exs
+++ mix.exs
@@ -36,7 +36,8 @@ defmodule RallyApi.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [ {:httpoison, "~> 0.9.0"},
-      {:poison, "~> 2.0"} ]
+      {:poison, "~> 2.0"},
+      {:exvcr, "~> 0.8.2", only: :test} ]
   end
 
   defp package do

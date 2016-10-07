3f58a6410e63ecdfadca5960d76a939e36933dde
jcartwright <jason@ensightenterprises.com>
2016-10-07 14:13:59 -0500

    Cleanup queryable_types

--- lib/rally_api/rally_query.ex
+++ lib/rally_api/rally_query.ex
@@ -21,10 +21,10 @@ defmodule RallyApi.RallyQuery do
     change: "change", changeset: "changeset", conversation_post: "conversationpost",
     defect: "defect", defect_suite: "defectsuite", hierarchical_requirement: "hierarchicalrequirement",
     iteration: "iteration", iteration_cumulative_flow_data: "iterationcumulativeflowdata",
-    milestone: "milestone",
-    portfolio_item: "portfolioitem", feature: "portfolioitem/feature", initiative: "portfolioitem/initiative",
-    preference: "preference", preliminary_estimate: "preliminaryestimate", project: "project",
-    project_permission: "projectpermission", recycle_bin_entry: "recyclebinentry", release: "release",
+    milestone: "milestone", portfolio_item: "portfolioitem", feature: "portfolioitem/feature",
+    initiative: "portfolioitem/initiative", preference: "preference",
+    preliminary_estimate: "preliminaryestimate", project: "project", project_permission: "projectpermission",
+    recycle_bin_entry: "recyclebinentry", release: "release",
     release_cumulative_flow_data: "releasecumulativeflowdata", requirement: "requirement", revision: "revision",
     schedulable_artifact: "schedulableartifact", scm_repository: "scmrepository", state: "state",
     story: "hierarchicalrequirement", subscription: "subscription", tag: "tag",
@@ -32,8 +32,8 @@ defmodule RallyApi.RallyQuery do
     test_case_step: "testcasestep", test_folder: "testfolder", test_set: "testset",
     time_entry_item: "timeentryitem", time_entry_value: "timeentryvalue", type_definition: "typedefinition",
     user: "user", user_iteration_capacity: "useriterationcapacity", user_permission: "userpermission",
-    user_profile: "userprofile", workspace_configuration: "workspaceconfiguration",
-    workspace_permission: "workspacepermission", workspace: "workspace",
+    user_profile: "userprofile", workspace: "workspace", workspace_configuration: "workspaceconfiguration",
+    workspace_permission: "workspacepermission", 
   ]
   def queryable_types, do: @queryable_types
 

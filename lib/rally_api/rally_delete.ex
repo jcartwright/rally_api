d2aebb74265f0ab427957dd3a1fa2298d8544f07
jcartwright <jason@ensightenterprises.com>
2016-10-26 16:41:15 -0500

    Support delete by _ref

--- lib/rally_api/rally_delete.ex
+++ lib/rally_api/rally_delete.ex
@@ -1,13 +1,41 @@
 defmodule RallyApi.RallyDelete do
-  import RallyApi
+  # DO NOT import RallyApi - this causes an import conflict for delete/2
   import RallyApi.Rallyties, only: [deleteable_types: 0]
 
   alias RallyApi.OperationResult
 
+  @doc """
+  Deletes a Rally Object using the `_ref` provided
+
+  ## Examples:
+
+    RallyApi.RallyDelete.delete(client, "https://path/to/object/_object_id")
+
+  """
+  def delete(client, ref) do
+    case RallyApi.delete(client, ref) do
+      {:ok, response} ->
+        result = OperationResult.to_result(response)
+        if Enum.empty?(result.errors),
+        do:   {:ok, result},
+        else: {:error, Enum.at(result.errors, 0)}
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
+
+  @doc """
+  Deletes a Rally Object by specifying type and the ObjectID
+
+  ## Examples:
+
+    RallyApi.RallyDelete.delete(client, :defect, 123456789)
+
+  """
   def delete(client, type, object_id) do
     case path_for(type, object_id) do
       {:ok, path} ->
-        case delete(client, path) do
+        case RallyApi.delete(client, path) do
           {:ok, response} ->
             result = OperationResult.to_result(response)
             if Enum.empty?(result.errors),
--- test/rally_api/rally_delete_test.exs
+++ test/rally_api/rally_delete_test.exs
@@ -2,8 +2,8 @@ defmodule RallyApi.RallyDeleteTest do
   use ExUnit.Case, async: false
   use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
 
-  import RallyApi.{TestHelper, RallyCreate, RallyDelete}
-  alias RallyApi.{Client, CreateResult, OperationResult}
+  import RallyApi.{TestHelper, RallyCreate, RallyDelete, RallyQuery}
+  alias RallyApi.{Client, CreateResult, OperationResult, QueryResult}
   
   @client Client.new(%{zsessionid: Application.get_env(:rally_api, :api_key)})
 
@@ -12,7 +12,20 @@ defmodule RallyApi.RallyDeleteTest do
     :ok
   end
 
-  test "delete a defect" do
+  test "delete/2 by _ref" do
+    use_cassette "rally_delete#delete_defect_by_ref", match_requests_on: [:query] do
+      {:ok, %CreateResult{object: defect}} = create(@client, :defect, simple_defect_attributes)
+      ref = defect["_ref"]
+
+      {:ok, %OperationResult{object: deleted}} = delete(@client, ref)
+      assert deleted == nil
+
+      {:ok, %{"OperationResult" => %{"Errors" =>  errors}}} = RallyApi.Defects.read(@client, ref)
+      assert List.first(errors) == "Cannot find object to read"
+    end
+  end
+
+  test "delete/3" do
     use_cassette "rally_delete#delete_defect" do
       {:ok, %CreateResult{object: defect}} = create(@client, :defect, simple_defect_attributes)
       object_id = defect["ObjectID"]
@@ -26,7 +39,7 @@ defmodule RallyApi.RallyDeleteTest do
     end
   end
 
-  test "delete fails for unknown id" do
+  test "delete/3 fails for unknown id" do
     use_cassette "rally_delete#unknown_id" do
       {:error, reason} = delete(@client, :defect, "this_is_not_valid")
       assert reason =~ ~r/^Could not read object/
